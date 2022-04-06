0fbd2ded607f621081b06ca5bb211c9aa886cbdc
nachogl1 <ignacio.gonzalez@codurance.com>
2022-04-06 13:27:06 +0100

    first commit

---
+++ src/test/java/com/katas/katas/KatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katas.katas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/fizzbuzz/FizzBuzzGeneratorShould.java
@@ -0,0 +1,15 @@
+package fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class FizzBuzzGeneratorShould {
+	
+	@Test
+	public void miauTest() {
+		
+	}
+
+}
