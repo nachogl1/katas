12a29d8ef4966da3004f74c52724097ee8d300f4
nachogl1 <ignacio.gonzalez@codurance.com>
2022-04-12 15:43:04 +0100

    refactoring roman numbers kata

--- src/main/java/com/katas/RomanNumberParserV1/RomanNumberParserSG.java
+++ src/main/java/com/katas/RomanNumberParserV1/RomanNumberParserSG.java
@@ -1,16 +1,26 @@
 package com.katas.RomanNumberParserV1;
 
 public class RomanNumberParserSG {
-    public String parse(int i) {
+    public String parse(int source) {
         String result = "";
         for (RomanNumbersEnum number : RomanNumbersEnum.values()) {
-            while (i >= number.getArabicFormat()) {
-                i -= number.getArabicFormat();
-                result = result+ number;
+            while (source >= number.getArabicFormat()) {
+                source = substract(source, number);
+                result = add(result, number);
             }
 
         }
         return result;
+    }
+
+    private String add(String result, RomanNumbersEnum number) {
+        result += number;
+        return result;
+    }
+
+    private int substract(int source, RomanNumbersEnum number) {
+        source -= number.getArabicFormat();
+        return source;
     }
 
 }
