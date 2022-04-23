bf3574dc66273110470ce1a80b21f81618f76bc7
nachogl1 <ignacio.gonzalez@codurance.com>
2022-04-23 18:27:55 +0100

    TDD TIC TAC TOE REFACTOR

--- src/test/java/com/katas/TicTacToeGameTest/TicTacToeGameShould.java
+++ src/test/java/com/katas/TicTacToeGameTest/TicTacToeGameShould.java
@@ -1,66 +1,199 @@
 package com.katas.TicTacToeGameTest;
 
-import com.katas.ticTacToeGame.TicTacToeGame;
+import com.katas.ticTacToeGame.TTTGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
-import javax.transaction.NotSupportedException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 
-//OLD
 public class TicTacToeGameShould {
-//quick win not detected..
-    //too many elemtns in my class
-    public static final String SEPARATOR = ":";
-    public static final String NO_PLAYER = "NONE";
+
     public static final String PLAYER_X = "X";
     public static final String PLAYER_O = "O";
-    private TicTacToeGame ticTacToeGame;
+    private TTTGame tttGame;
 
     @BeforeEach
     void setUp() {
-        ticTacToeGame = new TicTacToeGame();
+        tttGame = new TTTGame();
     }
 
     @Test
-    void return_true_if_game_valid() {
+    void return_player_X_as_the_winner_of_the_first_column() {
 
-        String moves = "1|2|3|4|5|6|7|8|9";
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 1);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 4);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 3);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 7);
 
-        String game = PLAYER_X + SEPARATOR + moves;
-        ticTacToeGame.initGame(game);
-        boolean result = ticTacToeGame.isValid();
+        tttGame.checkRules();
 
-        assertThat(result).isTrue();
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("X");
     }
 
-    @ParameterizedTest
-    @CsvSource({"X,1|2|3|4|5|6|7|8|9|1", "X,1|2|3|4|5|6|7|8", "M,1|2|3|4|5|6|7|8|9", "X,1|2|3|4|5|6|7|8|P", "X,1|2|3|4|5|6|7|.|9", "X,1|2|3|4|5|6|7|8|8"})
-    void return_false_if_game_not_valid(String firstPlayerToMove, String moves) {
+    @Test
+    void return_player_O_as_the_winner_of_the_first_column() {
 
-        String game = firstPlayerToMove + SEPARATOR + moves;
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 1);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 4);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 3);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 7);
 
-        ticTacToeGame.initGame(game);
-        boolean result = ticTacToeGame.isValid();
+        tttGame.checkRules();
 
-        assertThat(result).isFalse();
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("O");
     }
 
-    @ParameterizedTest
-    @CsvSource({"O,1|2|4|3|7|5|8|6|9"})
-    void be_valid_and_return_winner_if_column_completed(String firstPlayerToMove, String moves) throws NotSupportedException {
-        String game = firstPlayerToMove + SEPARATOR + moves;
+    @Test
+    void return_player_O_as_the_winner_of_the_second_column() {
 
-        ticTacToeGame.initGame(game);
-        boolean result = ticTacToeGame.isValid();
-        String winner = ticTacToeGame.checkWinner();
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 1);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 5);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 3);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 8);
 
-        assertThat(result).isTrue();
-        assertThat(winner).isEqualTo(PLAYER_X);
+        tttGame.checkRules();
 
+
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("O");
+    }
+
+
+    @Test
+    void return_player_X_as_the_winner_of_the_third_column() {
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 3);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 6);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 5);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 9);
+
+        tttGame.checkRules();
+
+
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("X");
+    }
+
+    @Test
+    void return_player_X_as_the_winner_of_the_first_row() {
+
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 1);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 4);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 5);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 3);
+
+        tttGame.checkRules();
+
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("X");
+    }
+
+    @Test
+    void return_player_O_as_the_winner_of_the_third_row() {
+
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 7);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 1);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 8);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 9);
+
+        tttGame.checkRules();
+
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("O");
+    }
+
+    @Test
+    void return_player_O_as_the_winner_of_top_left_to_right_diagonal() {
+
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 1);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 5);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 3);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 9);
+        tttGame.checkRules();
+
+
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("O");
+    }
+
+    @Test
+    void return_player_X_as_the_winner_of_top_right_to_left_diagonal() {
+
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 3);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 1);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 5);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 7);
+        tttGame.checkRules();
+
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("X");
+    }
+
+    @Test
+    void return_none_if_no_winner() {
+
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 2);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 1);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 4);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 3);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 5);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 6);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 7);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 8);
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 9);
+        tttGame.checkRules();
+
+        boolean isGameDone = tttGame.isGameDone();
+        String winner = tttGame.getWinner();
+
+        assertThat(isGameDone).isTrue();
+        assertThat(winner).isEqualTo("NONE");
+    }
+
+    @Test()
+    void return_exception_if_cell_taken() {
+
+        tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_X, 2);
+
+        assertThatIllegalStateException().isThrownBy(() -> {
+            tttGame.makeMoveFor(TicTacToeGameShould.PLAYER_O, 2);
+        });
 
     }
+
+
 }
