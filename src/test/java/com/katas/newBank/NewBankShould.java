d7a9be55869cb095c9b9a7eb4c901c9c38047f79
Ignacio Gonzalez <ignacio.gonzalez@codurance.com>
2022-09-09 11:01:46 +0100

    Starting NewBank Kata with Istvan

---
+++ src/test/java/com/katas/newBank/NewBankShould.java
@@ -0,0 +1,29 @@
+package com.katas.newBank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class NewBankShould {
+
+    @Test
+    void givenPredefinedSetOfActions_WhenAccountIsNew_ThenCorrectBankStatementIsPrinted() {
+        StatementPrinter console = new StatementPrinter();//mock(StatementPrinter.class);
+        DateProvider dateProvider = mock(DateProvider.class);
+        PersonalAccountService accountService = new PersonalAccountService(console, dateProvider);
+        when(dateProvider.getCurrentDate()).thenReturn("10/01/2012", "13/01/2012", "14/01/2012");
+        String bankStatement = """
+                Date       || Amount || Balance
+                14/01/2012 || -500   || 2500
+                13/01/2012 || 2000   || 3000
+                10/01/2012 || 1000   || 1000
+                """;
+
+        accountService.deposit(1000);
+        accountService.deposit(2000);
+        accountService.withdraw(500);
+        accountService.printStatement();
+
+        verify(console, times(1)).printStatement(bankStatement);
+    }
+}
