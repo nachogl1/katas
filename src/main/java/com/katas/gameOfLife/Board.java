0b72513d72d51bb6e505f463510e3e619da939c9
Ignacio Gonzalez <ignacio.gonzalez@codurance.com>
2022-10-25 14:05:30 +0100

    fixing game of live v1

--- src/main/java/com/katas/gameOfLife/Board.java
+++ src/main/java/com/katas/gameOfLife/Board.java
@@ -2,7 +2,7 @@ package com.katas.gameOfLife;
 
 public class Board {
 
-    private boolean[][] initialBoard = new boolean[][]{
+    private boolean[][] currentBoard = new boolean[][]{
             {false, false, true, false, false},
             {false, true, true, true, false},
             {false, false, true, false, false}
@@ -11,38 +11,54 @@ public class Board {
     private Detector detector = new Detector();
 
     public Board() {
-        nextGenBoard = new boolean[this.initialBoard.length][this.initialBoard[0].length];
+        nextGenBoard = new boolean[this.currentBoard.length][this.currentBoard[0].length];
     }
 
     public boolean[][] makeNextGenBoard() {
         processRows();
         assignBoards();
 
-        return this.initialBoard;
+        return this.currentBoard;
     }
 
     private void processRows() {
-        for (int row = 0; row < this.initialBoard.length; row++) {
+        for (int row = 0; row < this.currentBoard.length; row++) {
             processColumns(row);
         }
     }
 
     private void processColumns(int row) {
-        for (int column = 0; column < this.initialBoard[0].length; column++) {
+        for (int column = 0; column < this.currentBoard[0].length; column++) {
             nextGenBoard[row][column] = isCellAliveForNextGen(row, column);
         }
     }
 
     private void assignBoards() {
-        this.initialBoard = this.nextGenBoard;
-        this.nextGenBoard = new boolean[this.initialBoard.length][this.initialBoard[0].length];
+        this.currentBoard = this.nextGenBoard;
+        this.nextGenBoard = new boolean[this.currentBoard.length][this.currentBoard[0].length];
     }
 
     private boolean isCellAliveForNextGen(int row, int column) {
-        return isAliveCountEqualTo(row, column, 2) || isAliveCountEqualTo(row, column, 3);
+        return isAliveWith2or3Neighbors(row, column) || isDeadWith3Neighbors(row, column);
+    }
+
+    private boolean isDeadWith3Neighbors(int row, int column) {
+        return !isCellUnderStudyAlive(row, column) && isAliveCountEqualTo(row, column, 3);
+    }
+
+    private boolean isAliveWith2or3Neighbors(int row, int column) {
+        return isCellUnderStudyAlive(row, column) && hasTwoOrThreeNeighbors(row, column);
+    }
+
+    private boolean isCellUnderStudyAlive(int row, int column) {
+        return this.currentBoard[row][column];
+    }
+
+    private boolean hasTwoOrThreeNeighbors(int row, int column) {
+        return (isAliveCountEqualTo(row, column, 3)) || isAliveCountEqualTo(row, column, 2);
     }
 
     private boolean isAliveCountEqualTo(int row, int column, int value) {
-        return detector.detectAliveNeighbours(row, column, this.initialBoard) == value;
+        return detector.detectAliveNeighbours(row, column, this.currentBoard) == value;
     }
 }
